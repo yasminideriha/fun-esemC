0754facb72dd2e979399b07ae301e1300f01ff81
yasminideriha <162131402+yasminideriha@users.noreply.github.com>
2024-03-04 16:17:34 +0000

    funcao01

---
+++ .ccls-cache/@home@runner@funcoes-1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void dizerboatarde (char nome [20]){
+  printf("boa tarde!\n %s\n", nome);  
+}
+void dizerboatarde2 (char nome [20]){
+ 
+}
+int main(void) {
+  dizerboatarde("yasmin");
+  dizerboatarde("astrubal");
+  return 0;
+}
